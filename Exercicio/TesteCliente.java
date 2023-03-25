b7664f84d6e16df516029295e74a181f2219413d
Juliana_Inocencio <jinocencio0911@gmail.com>
2023-03-25 12:40:55 -0300

    Exercicio 1 - POO

---
+++ Exercicio/TesteCliente.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cliente cliente1 = new Cliente("Juliana","Inocencio", 28,1234567891,1994);
+		
+		cliente1.visualizar();
+	}
+
+}

fa5e9dbf6ca43afe92f68f0754d8c2de82906695
Juliana_Inocencio <jinocencio0911@gmail.com>
2023-03-27 11:05:05 -0300

    Exercicio Herança

--- Exercicio/TesteCliente.java
+++ Exercicio/TesteCliente.java
@@ -6,8 +6,13 @@ public class TesteCliente {
 		// TODO Auto-generated method stub
 		
 		Cliente cliente1 = new Cliente("Juliana","Inocencio", 28,1234567891,1994);
+		PessoaFisica pessoa1 = new PessoaFisica("Juliana","Inocencio", 28,1234567891,1994,"R. José de Anchieta, n. 122");
+		PessoaJuridica pessoaJuridica = new PessoaJuridica("Juliana","Inocencio", 28,1234567891,1994,"22222222/000122");
 		
 		cliente1.visualizar();
+		pessoa1.visualizar();
+		pessoaJuridica.visualizar();
+		
 	}
 
 }
